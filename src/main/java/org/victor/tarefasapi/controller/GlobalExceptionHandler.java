8a65a867caec1e9c88b61f07afd924c2ddfaf0f6
Victor <victorrafaelravazi1234@gmail.com>
2026-09-09 16:39:55 -0300

    Adicionada classe que cuida das exceptions

---
+++ src/main/java/org/victor/tarefasapi/controller/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package org.victor.tarefasapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> tratarValidacao(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(erro -> erros.put(erro.getField(), erro.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(erros);
+    }
+}
