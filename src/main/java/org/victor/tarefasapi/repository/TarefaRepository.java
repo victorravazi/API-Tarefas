e5488e8dead8c1d25c7bb3f75a59d5570c0eff4a
Victor <victorrafaelravazi1234@gmail.com>
2026-09-09 15:57:37 -0300

    Implementando Jpa

--- src/main/java/org/victor/tarefasapi/repository/TarefaRepository.java
+++ src/main/java/org/victor/tarefasapi/repository/TarefaRepository.java
@@ -1,53 +1,12 @@
 package org.victor.tarefasapi.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.victor.tarefasapi.model.TarefaModel;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Repository
-public class TarefaRepository {
-    List<TarefaModel> tarefas = new ArrayList<>();
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
 
-    public void salvarTarefa(TarefaModel tarefa){
-        tarefas.add(tarefa);
-    }
-
-    public List<TarefaModel> listarTarefas(){
-        return tarefas;
-    }
-
-    public TarefaModel buscarTarefaPorId(int id) {
-        for (TarefaModel tarefa : tarefas) {
-            if (tarefa.getID() == id) {
-                return tarefa;
-            }
-        }
-        return null;
-    }
-
-    public boolean atualizarTarefaPorId(String titulo,String descricao, int id){
-        for (TarefaModel tarefa : tarefas){
-            if (tarefa.getID() == id){
-                tarefa.setTitulo(titulo);
-                tarefa.setDescricao(descricao);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean deletarTarefaPorId(int id){
-        Iterator<TarefaModel> iterator = tarefas.iterator();
-        while(iterator.hasNext()) {
-            TarefaModel tarefa = iterator.next();
-            if (tarefa.getID() == id) {
-                iterator.remove();
-                return true;
-            }
-        }
-        return false;
-    }
 }
